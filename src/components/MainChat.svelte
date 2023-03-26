43e5a3e061a31c9ef15e3be29458ca5ec30f3af1
barrotbake <nguyenjacob411@gmail.com>
2023-03-26 15:37:23 -0700

    html for main chat

---
+++ src/components/MainChat.svelte
@@ -0,0 +1,17 @@
+<script>
+
+</script>
+
+<div class="main-container">
+    <div class="chat-box">
+        <!-- Chat messages will be rendered here -->
+    </div>
+    <form action="">
+        <input type="text" name="" id="">
+        <button type="submit">Send</button>
+    </form>
+</div>
+
+<style>
+
+</style>

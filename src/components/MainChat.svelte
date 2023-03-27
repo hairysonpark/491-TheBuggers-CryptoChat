e9edfd711848bc21fa208684f15b17cba0ba309d
barrotbake <nguyenjacob411@gmail.com>
2023-03-27 11:28:05 -0700

    Send chat function and messageHistory store

--- src/components/MainChat.svelte
+++ src/components/MainChat.svelte
@@ -1,14 +1,40 @@
 <script>
+    import { setContext } from 'svelte';
+    import { writable } from 'svelte/store';
+    import ChatMessage from './ChatMessage.svelte';
+    let chatHistory = writable([]);
+    setContext('chatHistory', chatHistory);
+
+    let getMessage = () => {
+        let message = {
+            text: document.querySelector('input[name=messageText]').value,
+            time: "12:00 pm",
+            user: "Joe"
+        }
+        return message;
+    }
+
+    let sendMessage = () => {
+        let message = getMessage();
+        chatHistory.update((messages) => {
+            return [...messages, message];
+        });
+    }
+
+
 
 </script>
 
 <div class="main-container">
     <div class="chat-box">
         <!-- Chat messages will be rendered here -->
+        {#each $chatHistory as message}
+            <ChatMessage messageText={message.text} messageTime={message.time} messageUser={message.user} />
+        {/each}
     </div>
-    <form action="">
-        <input type="text" name="" id="">
-        <button type="submit">Send</button>
+    <form>
+        <input type="text" name="messageText">
+        <button type="submit" on:click={sendMessage}>Send</button>
     </form>
 </div>
 
